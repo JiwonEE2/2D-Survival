683580ac66dc6e8081a810203e16f1b899968ff1
Nori <jiwonchichi1@gmail.com>
2024-10-22 10:45:34 +0900

    241022 10:45 과제 풀이 완

--- Assets/Space Survival/Scripts/Enemy.cs
+++ Assets/Space Survival/Scripts/Enemy.cs
@@ -5,50 +5,58 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    //public float maxHp = 10f;//하수
-    private float maxHp;
-    public float hp = 10f; //체력
-    public float damage = 10f; //공격력
-    public float moveSpeed = 3f; //이동 속도
+	//public float maxHp = 10f;//하수
+	private float maxHp;
+	public float hp = 10f; //체력
+	public float damage = 10f; //공격력
+	public float moveSpeed = 3f; //이동 속도
 
-    //초고수
-    public float hpAmount { get { return hp / maxHp; } } //자주 계산되는 항목은 프로퍼티로 만들기
-    //Getter/Setter
+	//초고수
+	public float hpAmount { get { return hp / maxHp; } } //자주 계산되는 항목은 프로퍼티로 만들기
 
-    private Transform target; //추적할 대상
+	//Getter/Setter
 
-    public Image hpBar;
+	private Transform target; //추적할 대상
 
-    private void Start()
-    {
-        target = GameObject.Find("Player").transform;
-        maxHp = hp;
-    }
+	public Image hpBar;
 
-    private void Update()
-    {
-        Vector2 moveDir = target.position - transform.position;
-        Move(moveDir.normalized);
-        //print(moveDir.magnitude);//vector.magnitude:해당 벡터가 "방향벡터"로 간주될 때, 벡터의 길이
-        //print(moveDir.normalized);//방향을 유지한채 길이가 1로 고정된 벡터.
-        hpBar.fillAmount = hpAmount;
-    }
+	private void Start()
+	{
+		target = GameObject.Find("Player").transform;
+		maxHp = hp;
+	}
 
-    public void Move(Vector2 dir)//dir 값이 커져도 1로 고정을 하고 싶을경우=>normalized
-    {
-        transform.Translate(dir * moveSpeed * Time.deltaTime);
-    }
+	private void Update()
+	{
+		Vector2 moveDir = target.position - transform.position;
+		Move(moveDir.normalized);
+		//print(moveDir.magnitude);//vector.magnitude:해당 벡터가 "방향벡터"로 간주될 때, 벡터의 길이
+		//print(moveDir.normalized);//방향을 유지한채 길이가 1로 고정된 벡터.
+		hpBar.fillAmount = hpAmount;
+	}
 
-    //OnHit,
-    public void TakeDamage(float damage)
-    {
-        hp -= damage;
+	public void Move(Vector2 dir)//dir 값이 커져도 1로 고정을 하고 싶을경우=>normalized
+	{
+		transform.Translate(dir * moveSpeed * Time.deltaTime);
+	}
 
-        if(hp <= 0) //으앙 쥬금
-        {
-            Destroy(gameObject);
-        }
+	//OnHit, ...
+	public void TakeDamage(float damage)
+	{
+		hp -= damage;
 
-    }
+		if (hp <= 0) //으앙 쥬금
+		{
+			GameObject.Find("Player").GetComponent<Player>().killCount++;
+			Destroy(gameObject);
+		}
+	}
 
+	private void OnCollisionStay2D(Collision2D collision)
+	{
+		if (collision.collider.CompareTag("Player"))
+		{
+			collision.collider.GetComponent<Player>().TakeDamage(damage);
+		}
+	}
 }
--- Assets/Space Survival/Scripts/Player.cs
+++ Assets/Space Survival/Scripts/Player.cs
@@ -1,60 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    public float hp = 100f; //체력
-    public float damage = 5f; //공격력
-    public float moveSpeed = 5f; //이동속도
+	private float maxHp;
+	public float hp = 100f; //체력
+	public float damage = 5f; //공격력
+	public float moveSpeed = 5f; //이동속도
 
-    public Projectile projectilePrefab; //투사체 프리팹
+	public Projectile projectilePrefab; //투사체 프리팹
 
-    void Start()
-    {
-        
-    }
+	public float HpAmount { get => hp / maxHp; }    // 현재 체력 비율
 
-    void Update()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+	public int killCount = 0;
+	public Text killCountText;
+	public Image hpBarImage;
 
-        Vector2 dir = new Vector2(x, y);
+	private Transform moveDir;
+	private Transform fireDir;
 
-        Move(dir);
+	private void Awake()
+	{
+		moveDir = transform.Find("MoveDir");
+		fireDir = transform.Find("FireDir");
+	}
 
-        if (Input.GetButtonDown("Fire1"))
-        {
-            //Vector3 -> Vector2로 캐스팅 할 때 : z값이 생략
-            Vector2 mousePos = Input.mousePosition;
-            Vector2 mouseScreenPos = Camera.main.ScreenToWorldPoint(mousePos);
-            Vector2 fireDir = mouseScreenPos - (Vector2)transform.position;
+	void Start()
+	{
+		maxHp = hp;   // 최대 체력 지정
+	}
 
-            Fire(fireDir);
-        }
+	void Update()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
 
-    }
+		Vector2 moveDir = new Vector2(x, y);
 
-    /// <summary>
-    /// Transform을 통해 게임 오브젝트를 움직이는 함수.
-    /// </summary>
-    /// <param name="dir">이동 방향</param>
-    public void Move(Vector2 dir)
-    {
-        transform.Translate(dir * moveSpeed * Time.deltaTime);
-    }
+		Vector2 mousePos = Input.mousePosition;
+		Vector2 mouseScreenPos = Camera.main.ScreenToWorldPoint(mousePos);
+		Vector2 fireDir = mouseScreenPos - (Vector2)transform.position;
 
-    /// <summary>
-    /// 투사체를 발사.
-    /// </summary>
-    public void Fire(Vector2 dir)
-    {
-        Projectile projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+		//Vector3 -> Vector2로 캐스팅 할 때 : z값이 생략
+		Move(moveDir);
 
-        projectile.transform.up = dir;
-        projectile.damage = damage;
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Fire(fireDir);
+		}
 
-    }
+		killCountText.text = killCount.ToString();
+		hpBarImage.fillAmount = HpAmount;
 
+		// transform.up/right/forward에 방향 벡터를 대입할 때는 방향 벡터의 magnitude를 굳이 1로 제한하지 않아도 된다
+		this.moveDir.up = moveDir;
+		this.fireDir.up = fireDir;
+
+		//print(this.moveDir.up);		// normalized되어 magnitude가 1로 고정된 방향 벡터가 반환됨
+	}
+
+	/// <summary>
+	/// Transform을 통해 게임 오브젝트를 움직이는 함수.
+	/// </summary>
+	/// <param name="dir">이동 방향</param>
+	public void Move(Vector2 dir)
+	{
+		transform.Translate(dir * moveSpeed * Time.deltaTime);
+	}
+
+	/// <summary>
+	/// 투사체를 발사.
+	/// </summary>
+	public void Fire(Vector2 dir)
+	{
+		Projectile projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+		projectile.transform.up = dir;
+		projectile.damage = damage;
+	}
+
+	public void TakeDamage(float damage)
+	{
+		print($"아야! : {damage}");
+		hp -= damage;
+		if (hp <= 0)
+		{
+			// 게임 오버 처리
+
+		}
+	}
 }

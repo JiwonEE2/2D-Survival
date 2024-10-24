09a37995d878762f2f67fa1bf5357b1142a2dd15
JiwonEE2 <jiwonchichi1@gmail.com>
2024-10-24 10:22:24 +0900

    241024 10:22 적 스폰 위치를 Vector2.zero가 아닌, 플레이어 기준 특정 거리 이상 위치에 스폰하기 구현

--- Assets/Space Survival/Scripts/Systems/EnemySpawner.cs
+++ Assets/Space Survival/Scripts/Systems/EnemySpawner.cs
@@ -9,6 +9,8 @@ public class EnemySpawner : MonoBehaviour
 	// 2. 적 스폰 위치를 Vector2.zero가 아닌, 플레이어 기준 특정 거리 이상 위치에 스폰하기
 	[Tooltip("한번에 스폰될 적의 수\nx:최소, y:최대")]
 	public Vector2Int minMaxCount;
+	[Tooltip("스폰될 때 플레이어로부터의 최대/최소 거리\nx:최소, y:최대")]
+	public Vector2 minMaxDist;
 
 	public GameObject enemyPrefab;      // 적 프리팹
 	public float spawnInterval;         // 생성 간격
@@ -33,7 +35,20 @@ public class EnemySpawner : MonoBehaviour
 		for (int i = 0; i < count; i++)
 		{
 			Vector2 playerPos = GameManager.Instance.player.transform.position;
-			Instantiate(enemyPrefab);
+			// Random.insideUnitCircle : 길이가 1인 원 안에서 랜덤 위치의 좌표를 반환
+			Vector2 spawnPos;
+			int loopCount = 0;
+			float circleSize;
+			// 1. 일단 랜덤 좌표를 받은 후, 거리가 3 이내면 다시 랜덤을 받음
+			circleSize = Random.Range(minMaxDist.x, minMaxDist.y);
+			// 플레이어로부터의 거리가 0~minMaxDist.y 사이의 좌표를 받음
+			spawnPos = Random.insideUnitCircle * circleSize;
+
+			// 한 번에 스폰하는 경우도 있지만 조건이 맞지 않는 좌표가 나와서 루프 도는 빈도가 높아 보인다.
+			print($"Random Loop Count : {loopCount}");
+
+			// 랜덤으로 나온 좌표를 무조건 조건에 맞게 가공하도록 연산해서 활용해보기
+			Instantiate(enemyPrefab, playerPos + spawnPos, Quaternion.identity);
 		}
 	}
 }

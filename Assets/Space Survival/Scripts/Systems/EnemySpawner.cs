8e576c571ece8ecba485dd967ddf5ea690c224b9
JiwonEE2 <jiwonchichi1@gmail.com>
2024-10-24 09:39:25 +0900

    241024 9:39 몬스터 랜덤 스폰 구현 중

--- Assets/Space Survival/Scripts/Systems/EnemySpawner.cs
+++ Assets/Space Survival/Scripts/Systems/EnemySpawner.cs
@@ -5,6 +5,11 @@ using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
+	// 1. 적이 한 번 스폰될 때 1마리가 아닌 2~10마리 스폰하도록 변경
+	// 2. 적 스폰 위치를 Vector2.zero가 아닌, 플레이어 기준 특정 거리 이상 위치에 스폰하기
+	[Tooltip("한번에 스폰될 적의 수\nx:최소, y:최대")]
+	public Vector2Int minMaxCount;
+
 	public GameObject enemyPrefab;      // 적 프리팹
 	public float spawnInterval;         // 생성 간격
 
@@ -18,7 +23,11 @@ public class EnemySpawner : MonoBehaviour
 		while (true)
 		{
 			yield return new WaitForSeconds(spawnInterval);
-			Spawn();		// 몬스터 스폰
+			int enemyCount = Random.Range(minMaxCount.x, minMaxCount.y);
+			for (int i = 0; i < enemyCount; i++)
+			{
+				Spawn();    // 몬스터 스폰
+			}
 		}
 	}
 

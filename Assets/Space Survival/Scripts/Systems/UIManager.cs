1634a549fa41540a53fa329f877f8eaaa36a37c8
JiwonEE2 <jiwonchichi1@gmail.com>
2024-10-25 11:28:51 +0900

    241024 11:28 카메라 작업 완료 및 UI매니저 구현

---
+++ Assets/Space Survival/Scripts/Systems/UIManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// UI를 관리하는 싱글톤 오브젝트
+public class UIManager : MonoBehaviour
+{
+	public static UIManager Instance;
+}
